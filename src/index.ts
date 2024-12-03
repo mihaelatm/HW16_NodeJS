6508fec65db27d585bd76c19d0779e51306c429e
Talmazan Mihaela <talmazanmihaela@gmail.com>
2024-12-03 21:02:31 +0100

    Added Task 2

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,24 @@
 // function greetUser(name: String): void {
 //   console.log(`Hello ${name}!`);
 // }
-
 // greetUser("Bob");
+
+// ========= Task_2 =========
+
+interface Person {
+  name: string;
+  age: number;
+  city: string;
+}
+
+function printPersonInfo(person: Person): void {
+  console.log(`Name: ${person.name}, age: ${person.age}, city: ${person.city}`);
+}
+
+const person: Person = {
+  name: "John",
+  age: 26,
+  city: "London",
+};
+
+printPersonInfo(person);
